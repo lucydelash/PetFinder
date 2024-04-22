b27c55962c1034dfa8b1c247866534eaf91ec786
lucydelash <lucyrudd614@gmail.com>
2024-04-22 12:36:51 -0400

    fixed pet errors

--- src/routes/petsData.jsx
+++ src/routes/petsData.jsx
@@ -1,6 +1,6 @@
 const petsData = [
   { id: 1, name: 'Fluffy', owner: 'John' },
-  { id: 2, name: 'Chimi The Changa', owner: 'Tyler' },
+  { id: 2, name: 'ChimiChanga', owner: 'Tyler' },
   { id: 3, name: 'Mowgli', owner: 'Anusha' },
   { id: 4, name: 'Charmander', owner: 'Char' }
 ];
